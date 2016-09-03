757eab010058c8da6fad4300ecfb4687fe846873
Ankit Mishra <anmishr@microsoft.com>
2016-09-03 00:55:53 -0700

    tested code

--- NupkgPs1Parser.cs
+++ NupkgPs1Parser.cs
@@ -1,46 +1,208 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Packaging;
+using NuGet.Packaging.Core;
 
 namespace Nuget.NupkgParser
 {
     internal class NupkgPs1Parser
     {
-        private static void Main(string[] args)
-        {
-            var folderPath = @"F:\MirrorPackages";
-            enumerateFiles(folderPath);
-        }
+        private string _inputPath;
+        private string _outputPath;
+        private string _logFile;
 
-        private static void enumerateFiles(string path)
+        private ConcurrentDictionary<string, ConcurrentDictionary<string, List<string>>> _packageCollection;
+
+        public NupkgPs1Parser(string inputPath, string outputPath, string logFile, string errorLogFile)
         {
-            foreach (string file in Directory.EnumerateFiles(path))
+            _inputPath = inputPath;
+            _outputPath = outputPath;
+            _logFile = logFile;
+            _packageCollection = new ConcurrentDictionary<string, ConcurrentDictionary<string, List<string>>>();
+            if (!File.Exists(_logFile))
             {
-                containsScripts(file);
+                File.Create(_logFile).Close();
             }
         }
 
-        private static bool containsScripts(string path)
+        private static void Main(string[] args)
         {
-            bool result = false;
-            openArchive(path);
-            return result;
+            NupkgPs1Parser nupkgParser = new NupkgPs1Parser(@"F:\MirrorPackages", @"F:\ProcessedPackages", @"F:\ProcessedPackages\log.csv", @"F:\ProcessedPackages\errors.txt");
+            nupkgParser.enumerateFiles();
+            Console.WriteLine("Writting results into log");
+            nupkgParser.populateResultsCsv();
         }
 
-        private static void openArchive(string path)
+        private void enumerateFiles()
         {
-            // Get id and version for nupkg
-
-            // Copy nupkg into v3 style structure
-
-            // Read nupkg for ps1's and extract the nupkgs into the same folder as nupkg
-            var archive = ZipFile.Open(path, ZipArchiveMode.Read);
-            foreach (var entry in archive.Entries)
+            try
             {
-                if (entry.FullName.EndsWith(".ps1", StringComparison.OrdinalIgnoreCase))
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+
+                var inputFiles = Directory.EnumerateFiles(_inputPath).ToArray();
+
+                long count = 0;
+                long errorCount = 0;
+                ParallelOptions ops = new ParallelOptions() { MaxDegreeOfParallelism = 16 };
+                Parallel.ForEach(inputFiles, file =>
                 {
-                    Console.WriteLine(string.Concat("ps1 - ", entry.Name, " in nupkg - ", Path.GetFileName(path)));
+                    count++;
+                    if (count % 5000 == 0)
+                    {
+                        Console.WriteLine("Done with " + count + " packages with " + errorCount + " errors");
+
+                        double percentDone =  (double)count / (double)inputFiles.Length;
+
+                        var timeLeft = ((timer.Elapsed.TotalSeconds / count) * inputFiles.Length) - timer.Elapsed.TotalSeconds;
+
+                        var dateEnd = DateTime.Now.AddSeconds(timeLeft);
+                        var timeLeftSpan = TimeSpan.FromSeconds(timeLeft);
+
+                        Console.WriteLine($"Done in {timeLeftSpan} at {dateEnd}");
+                    }
+                    try
+                    {
+                        processArchive(file);
+                    }
+                    catch (Exception ex)
+                    {
+                        errorCount++;
+                        Console.WriteLine("Exception while parsing " + file);
+                        Console.WriteLine(ex.Message);
+                        logError(Path.GetFileName(file), Path.Combine(_outputPath, @"error_" + Thread.CurrentThread.ManagedThreadId + ".txt"));
+                    }
+                });
+                Console.WriteLine("Done with all the" + count + " packages");
+            }
+            catch (AggregateException ae)
+            {
+                // This is where you can choose which exceptions to handle.
+                foreach (var ex in ae.InnerExceptions)
+                {
+                    Console.WriteLine(ex.Message);
                 }
+            }
+        }
+
+        private void processArchive(string path)
+        {
+            using (var archive = ZipFile.Open(path, ZipArchiveMode.Read))
+            {
+                var ps1Files = archive.Entries
+                    .Where(e => e.FullName.EndsWith(".ps1", StringComparison.OrdinalIgnoreCase))
+                    .Select(e => e.FullName)
+                    .ToArray();
+
+                if (ps1Files.Length > 0)
+                {
+                    // Create V3 style location
+                    PackageArchiveReader packageReader = new PackageArchiveReader(archive);
+                    var packageIdentity = packageReader.GetIdentity();
+                    //var v3Path = createV3Folders(packageIdentity);
+                    //var newPath = Path.Combine(v3Path, Path.GetFileName(path));
+
+                    // Copy nupkg into v3 style structure
+                    //File.Copy(path, newPath);
+
+                    // Read nupkg for ps1's and extract the nupkgs into the same folder as nupkg
+
+                    foreach (var ps1FilePath in ps1Files)
+                    {
+                        addToPackageCollection(packageIdentity, ps1FilePath);
+                        //var filePath = Path.Combine(v3Path, entry.FullName);
+                        //createDirectory(Directory.GetParent(filePath).FullName);
+                        //entry.ExtractToFile(filePath);
+
+                        //log(string.Concat(packageIdentity.Id, " ", packageIdentity.Version, " ", entry.Name));
+                    }
+                }
+            }
+        }
+
+        private void addToPackageCollection(PackageIdentity packageIdentity, string fileName)
+        {
+            string id = packageIdentity.Id;
+            string version = packageIdentity.Version.ToString();
+            if (_packageCollection.ContainsKey(id))
+            {
+                if (_packageCollection[id].ContainsKey(fileName))
+                {
+                    _packageCollection[id][fileName].Add(version);
+                }
+                else
+                {
+                    _packageCollection[id][fileName] = new List<string> { version };
+                }
+            }
+            else
+            {
+                _packageCollection[id] = new ConcurrentDictionary<string, List<string>>();
+                _packageCollection[id][fileName] = new List<string> { version };
+            }
+        }
+
+        private void populateResultsCsv()
+        {
+            using (StreamWriter w = File.AppendText(_logFile))
+            {
+                foreach (var id in _packageCollection.Keys)
+                {
+                    var versions = new StringBuilder();
+                    var file = "";
+                    foreach (var fileName in _packageCollection[id].Keys)
+                    {
+                        file = fileName;
+                        versions.Append(_packageCollection[id][fileName]);
+                        versions.Append(" ");
+                    }
+                    w.WriteLine(string.Concat(id, ",", file, ",", versions));
+                }
+            }
+        }
+
+        private string createV3Folders(PackageIdentity packageIdentity)
+        {
+            var id = packageIdentity.Id;
+            var version = packageIdentity.Version;
+            var versionDer = (Path.Combine(_outputPath, id, version.ToString()));
+            createDirectory(versionDer);
+            return versionDer;
+        }
+
+        private void createDirectory(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        private void log(string line)
+        {
+            using (StreamWriter w = File.AppendText(_logFile))
+            {
+                w.WriteLine(line);
+            }
+        }
+
+        private void logError(string line, string errorFilePath)
+        {
+            if (!File.Exists(errorFilePath))
+            {
+                File.Create(errorFilePath).Close();
+            }
+            using (StreamWriter w = File.AppendText(errorFilePath))
+            {
+                w.WriteLine(line);
             }
         }
     }
